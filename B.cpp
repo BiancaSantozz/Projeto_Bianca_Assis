613462d27d8f94464569a18087f05cd6d40fc473
Bianca <biancaassis0147@gmail.com>
2026-09-21 12:25:50 -0300

    Adicionado o metodo MB3 na classe B

--- B.cpp
+++ B.cpp
@@ -6,19 +6,16 @@ private:
     float B2;
 
 public:
-    // Getters e Setters
     int getB1() { return B1; }
     void setB1(int b1) { B1 = b1; }
 
     float getB2() { return B2; }
     void setB2(float b2) { B2 = b2; }
 
-    // Métodos da classe
-    void MB1() { 
-        std::cout << "MB1" << std::endl; 
-    }
+    void MB1() { std::cout << "MB1" << std::endl; }
+    void MB2() { std::cout << "MB2" << std::endl; }
     
-    void MB2() { 
-        std::cout << "MB2" << std::endl; 
+    void MB3() { 
+        std::cout << "MB3" << std::endl; 
     }
 };
