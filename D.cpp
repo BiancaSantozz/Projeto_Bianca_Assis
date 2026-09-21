1a6f1747aecb27dbbdb4b980dcb6dceaf5adaf8c
Bianca <biancaassis0147@gmail.com>
2026-09-21 12:26:30 -0300

    Criacao da classe D

---
+++ D.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+class D {
+private:
+    double D1;
+    bool D2;
+
+public:
+    void MD1() { 
+        std::cout << "Metodo MD1 executado" << std::endl; 
+    }
+    
+    void MD2() { 
+        std::cout << "Metodo MD2 executado" << std::endl; 
+    }
+};
