a7d07d5dc9dbaee3748a17b47924931dbf1bf770
Bianca <biancaassis0147@gmail.com>
2026-09-21 10:51:57 -0300

    Criacao da Classe C

---
+++ C.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+class C {
+private:
+    std::string C1;
+    int C2;
+
+public:
+    // Getters e Setters
+    std::string getC1() { return C1; }
+    void setC1(std::string c1) { C1 = c1; }
+
+    int getC2() { return C2; }
+    void setC2(int c2) { C2 = c2; }
+
+    // Métodos
+    void MC1() { 
+        std::cout << "MC1" << std::endl; 
+    }
+    
+    void MC2() { 
+        std::cout << "MC2" << std::endl; 
+    }
+};
