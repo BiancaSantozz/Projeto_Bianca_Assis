b58b15927425400ac11acf1a146ce602b7cf5390
Deividy Terra <deividyterra2@gmail.com>
2026-09-22 10:27:34 -0300

    Método de Soma em A

--- A.cpp
+++ A.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 
-class A {
+class A
+{
 private:
-    int A1;
+    int A1, a, b;
     float A2;
 
 public:
@@ -14,15 +15,23 @@ public:
     void setA2(float a2) { A2 = a2; }
 
     // Métodos da classe
-    void MA1() { 
-        std::cout << "MA1" << std::endl; 
-    }
-    
-    void MA2() { 
-        std::cout << "MA2" << std::endl; 
+    void MA1()
+    {
+        std::cout << "MA1" << std::endl;
     }
 
-    void MA3() { 
-        std::cout << "Alteracao a classe A partir do clone" << std::endl; 
+    void MA2()
+    {
+        std::cout << "MA2" << std::endl;
+    }
+
+    void MA3()
+    {
+        std::cout << "Alteracao a classe A partir do clone" << std::endl;
+    }
+
+    void getSoma(int a, int b)
+    {
+        std::cout << a + b;
     }
 };
